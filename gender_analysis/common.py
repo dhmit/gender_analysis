787bb7851ea8673eba51cb12e7897dd42497eee7
Sophia Zhi <sophiazhi@gmail.com>
2019-06-18 14:46:06 -0400

    fixed hawthorne_scarlet doctest failure for mac
    not sure why it was failing before

--- gender_analysis/common.py
+++ gender_analysis/common.py
@@ -293,7 +293,8 @@ def get_text_file_encoding(filepath):
     For text file at filepath returns the text encoding as a string (e.g. 'utf-8')
 
     >>> from gender_analysis import common
-    >>> common.get_text_file_encoding(r"corpora/sample_novels/texts/hawthorne_scarlet.txt")
+    >>> path = r'corpora/sample_novels/texts/hawthorne_scarlet.txt'
+    >>> common.get_text_file_encoding(path)
     'UTF-8-SIG'
 
     Note: For files containing only ascii characters, this function will return 'ascii' even if
