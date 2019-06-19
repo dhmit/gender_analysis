3011de7853ac533d581a169e38e59ba8a87b7e1f
Ryaan Ahmed <ryaan.ahmed@gmail.com>
2019-06-19 11:51:49 -0400

    tidy the test runner a bit

--- gender_analysis/testing/doctests.py
+++ gender_analysis/testing/doctests.py
@@ -11,33 +11,32 @@ def print_test_header(test_name):
     print('================================================================================')
 
 
-# Recurse through all packages and return a list of all absolute submodule names
-def all_absolute_submodule_names(package, names):
+# Recurse through all packages and return a list of all submodules
+def get_all_submodules(package, module_list):
     subpackages = pkgutil.walk_packages(package.__path__)
     for module_info in subpackages:
-        absolute_module_name = package.__name__ + '.' + module_info.name
-
-        if 'corpus_gen' in absolute_module_name:
+        if 'corpus_gen' in module_info.name:
             # TODO(ra) skipping corpus_gen bc of Gutenberg installation issues...
             # we'll deal with this later...
             continue
 
+        absolute_module_name = package.__name__ + '.' + module_info.name
         module = importlib.import_module(absolute_module_name)
+
         if module_info.ispkg:
-            all_absolute_submodule_names(module, names)
+            get_all_submodules(module, module_list)
         else:
-            names.append(absolute_module_name)
+            module_list.append(module)
 
 if __name__ == '__main__':
-    submodule_names = []
-    all_absolute_submodule_names(gender_analysis, submodule_names)
+    modules = []
+    get_all_submodules(gender_analysis, modules)
 
     failures = 0
     tests = 0
 
-    for module_name in submodule_names:
-        print_test_header(module_name)
-        module = importlib.import_module(module_name)
+    for module in modules:
+        print_test_header(module.__name__)
         new_failures, new_tests = doctest.testmod(module)
         failures += new_failures
         tests += new_tests
