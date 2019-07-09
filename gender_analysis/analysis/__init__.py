f3e5972f78f284f1c17f544df6a773ca18fabace
Sophia Zhi <sophiazhi@gmail.com>
2019-07-09 12:49:00 -0400

    oops

--- gender_analysis/analysis/__init__.py
+++ gender_analysis/analysis/__init__.py
@@ -1,8 +1,8 @@
 __all__ = [
     'dunning',
-    'gender_pronoun_freq_analysis',
-    'instance_distance_analysis',
-    'pronoun_adjective_analysis',
+    'gender_pronoun_frequency',
+    'instance_distance',
+    'pronoun_adjective',
     'statistical',
 ]
 
