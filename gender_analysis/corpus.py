ce3287c97bc642a00a8b6389f4cca4bbfaedaa57
Samantha York <samimak@Samanthas-MacBook-Pro-3.local>
2019-07-11 12:00:24 -0400

    Update corpus.py
    Hopefully some of the last corpus edits

--- gender_analysis/corpus.py
+++ gender_analysis/corpus.py
@@ -314,6 +314,8 @@ class Corpus:
         """
         This function returns a Counter object that stores how many times each word appears in the corpus.
 
+        :return: Python Counter object
+
         >>> from gender_analysis.corpus import Corpus
         >>> from gender_analysis.common import BASE_PATH
         >>> path = BASE_PATH / 'testing' / 'corpora' / 'sample_novels' / 'texts'
