1e7f86acb7276c31ff20492beb03b50a30d5737d
Samantha York <samimak@Samanthas-MacBook-Pro-3.local>
2019-07-11 13:38:38 -0400

    Removed VSCode file
    VSCode inserted a settings file into the project, which is now gone.

--- gender_analysis/common.py
+++ gender_analysis/common.py
@@ -150,7 +150,6 @@ def get_text_file_encoding(filepath):
     >>> from gender_analysis import common
     >>> from pathlib import Path
     >>> import os
-
     >>> path=Path(common.TEST_DATA_PATH, 'sample_novels', 'texts', 'hawthorne_scarlet.txt')
     >>> common.get_text_file_encoding(path)
     'UTF-8-SIG'
