d6c2b892e40389ad956a3abfc3e371876b774a32
Sophia Zhi <sophiazhi@gmail.com>
2019-07-12 14:56:34 -0400

    Update gender_adjective.py

--- gender_analysis/analysis/gender_adjective.py
+++ gender_analysis/analysis/gender_adjective.py
@@ -183,7 +183,6 @@ def merge_raw_results(full_results):
     """
     merged_results = {'male': {}, 'female': {}}
     for novel in list(full_results.keys()):
-        print(novel.title, novel.author)
         for gender in list(full_results[novel].keys()):
             merged_results[gender] = merge(full_results[novel][gender], merged_results[gender])
 
