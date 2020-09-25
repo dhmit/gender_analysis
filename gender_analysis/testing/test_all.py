f4cc587bd9ad0f3dc9c3ee9bcc7de0c4987b3b96
Samantha York <samimak@mit.edu>
2020-09-25 12:06:47 -0800

    Allowed pytest to run doctests
    Removed old doctest files and moved over to the pytest runner

--- gender_analysis/testing/test_all.py
+++
@@ -1,8 +0,0 @@
-"""
-Runs all tests in the gender_analysis folder IF the
-module is imported in gender_analysis
-"""
-
-if __name__ == '__main__':
-    from dh_testers.singleCore import main
-    main()
