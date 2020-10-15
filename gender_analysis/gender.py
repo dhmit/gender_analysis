8f437867ba3a576840b35bc097342dfcb69a5abd
Samantha York <samimak@mit.edu>
2020-10-15 08:53:37 -0800

    Added name functions for Gender
    Allowed names to be associated with a gender, and added functions that allow a gender to be searched for them

--- gender_analysis/gender.py
+++ gender_analysis/gender.py
@@ -6,7 +6,7 @@ class Gender:
     Defines a gender that will be operated on in analysis functions
     """
 
-    def __init__(self, identifier, pronoun_series):
+    def __init__(self, identifier, pronoun_series, names=None):
         """
         Initializes a Gender object that can be used for comparing and contrasting in the
         analysis functions.
@@ -14,7 +14,10 @@ class Gender:
         The gender accepts one or more `PronounSeries` that will be used to identify the gender.
 
         :param identifier: String name of the gender
-        :param pronoun_series: `PronounSeries` or collection of `PronounSeries` that the gender uses.
+        :param pronoun_series: `PronounSeries` or collection of `PronounSeries` that the gender uses
+        :param names: A collection of names (as strings) that will be associated with the gender.
+            Note that the provided names are case-sensitive.
+
         """
 
         self.identifier = identifier
@@ -24,6 +27,16 @@ class Gender:
             self.pronoun_series = {pronoun_series}
         else:
             self.pronoun_series = set(pronoun_series)
+
+        # Manage names that are associated with the gender
+        self.names = set()
+        if names is not None:
+
+            # If the user inputs a single string, catch it
+            if type(names) == str:
+                self.names.add(names)
+            else:
+                self.names = set(names)
 
     def __repr__(self):
         """
@@ -61,7 +74,7 @@ class Gender:
     def __eq__(self, other):
         """
         Performs a check to see whether two `Gender`s are equivalent. This is true if and only
-        if the `Gender`s' identifiers and pronoun series are identical.
+        if the `Gender`s' identifiers, pronoun series, and names are identical.
 
         Note that this comparison works:
         >>> from gender_analysis.pronouns import PronounSeries
@@ -88,7 +101,8 @@ class Gender:
 
         return (
             self.identifier == other.identifier and
-            self.pronoun_series == other.pronoun_series
+            self.pronoun_series == other.pronoun_series and
+            self.names == other.names
         )
 
     def uses_pronoun(self, pronoun):
@@ -136,3 +150,51 @@ class Gender:
                 pronouns.add(pronoun)
 
         return pronouns
+
+    def uses_name(self, name):
+        """
+        Performs a check for whether the name is associated with the gender.
+
+        Note that names are case-sensitive.
+
+        >>> from gender_analysis.pronouns import PronounSeries
+        >>> from gender_analysis.gender import Gender
+        >>> he_series = PronounSeries('Masc', {'he', 'him', 'his'})
+        >>> masc_names = ['Andrew', 'Richard', 'Robert']
+        >>> male = Gender('Male', he_series, names=masc_names)
+        >>> male.uses_name('Richard')
+        True
+        >>> male.uses_name('robert')
+        False
+
+        :param name: A string name to search for
+        :return: A boolean indicating whether the name is associated with the gender
+        """
+
+        return name in self.names
+
+    def uses(self, identifier):
+        """
+        Checks to see whether the identifier is a name or pronoun that the gender is associated
+        with
+
+        >>> from gender_analysis.pronouns import PronounSeries
+        >>> from gender_analysis.gender import Gender
+        >>> he_series = PronounSeries('Masc', {'he', 'him', 'his'})
+        >>> masc_names = ['Andrew', 'Richard', 'Robert']
+        >>> male = Gender('Male', he_series, names=masc_names)
+        >>> male.uses('he')
+        True
+        >>> male.uses('Richard')
+        True
+        >>> male.uses('she')
+        False
+
+        :param identifier: A string that is either a pronoun or a name to check the gender for
+        :return: true if `identifier` is a pronoun or name that is used by the gender
+        """
+
+        return (
+            self.uses_name(identifier) or
+            self.uses_pronoun(identifier)
+        )
