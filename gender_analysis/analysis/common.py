b76fdec87b1678022b9e5b7669840dc00a0bfe7d
Ryaan Ahmed <mail@ryaanahmed.com>
2021-06-07 09:57:43 -0400

    lint

--- gender_analysis/analysis/common.py
+++ gender_analysis/analysis/common.py
@@ -1,5 +1,5 @@
 
-def compute_bin_year(year: int , time_frame_start: int, bin_size: int):
+def compute_bin_year(year: int, time_frame_start: int, bin_size: int):
     """
     Given an input year, the start of a time frame, and bin size,
     computes which bin start date this year belongs to.
